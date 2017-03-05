16bb4cb4caf8951920eb06fe64e078a793a57774
Constantine <cdiam@windowslive.com>
2017-03-05 20:20:38 +0200

    adding functions

--- CodeAcademy_Learning/script_py_for_codeacademy.py
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -217,3 +217,50 @@ def the_flying_circus():
 
     else:
         return "This is equal"
+
+the_flying_circus()
+
+
+
+###FUNCTION SECTIONS ####
+
+def tax(bill):
+    """Adds 8% tax to a restaurant bill."""
+    bill *= 1.08
+    print "With tax: %f" % bill
+    return bill
+
+def tip(bill):
+    """Adds 15% tip to a restaurant bill."""
+    bill *= 1.15
+    print "With tip: %f" % bill
+    return bill
+
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
+
+
+def square(n):
+    """Returns the square of a number."""
+    squared = n**2
+    print "%d squared is %d." % (n, squared)
+    return squared
+
+# Call the square function on line 9! Make sure to
+# include the number 10 between the parentheses.
+square(10)
+
+
+def power(base, exponent):  # Add your parameters here!
+    result = base**exponent
+    print "%d to the power of %d is %d." % (base, exponent, result)
+
+power(37,4)  # Add your arguments here!
+
+
+def one_good_turn(n):
+    return n + 1
+
+def deserves_another(n):
+    return one_good_turn(n) + 2
