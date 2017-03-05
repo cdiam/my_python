26a3c5d27683f84b3ea54eedd77cf85d6aaa7cff
Constantine <cdiam@windowslive.com>
2017-03-05 21:23:55 +0200

    Adding more lines of code

--- CodeAcademy_Learning/script_py_for_codeacademy.py
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+
 #This is the scripts from codeacademy
 #Set spam equal to 1 using modulo on line 3!
+
 
 spam =3%2
 
@@ -264,3 +267,297 @@ def one_good_turn(n):
 
 def deserves_another(n):
     return one_good_turn(n) + 2
+
+
+def cube(number):
+    return number**3
+
+def by_three(number):
+    if number % 3 ==0:
+      result =cube(number)
+      return result
+    else:
+        return False
+
+print cube(9)
+print by_three(9)
+
+
+def biggest_number(*args):
+    print max(args)
+    return max(args)
+
+def smallest_number(*args):
+    print min(args)
+    return min(args)
+
+def distance_from_zero(arg):
+    print abs(arg)
+    return abs(arg)
+
+
+biggest_number(-10, -5, 5, 10)
+smallest_number(-10, -5, 5, 10)
+distance_from_zero(-10)
+
+
+def distance_from_zero(argument):
+    if type(argument) == int or type(argument) == float:
+        return abs(argument)
+    else:
+        return "Nope"
+
+
+
+
+
+#### Python Lists and Dictionaries ###
+zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"];
+# One animal is missing!
+
+if len(zoo_animals) > 3:
+	print "The first animal at the zoo is the " + zoo_animals[0]
+	print "The second animal at the zoo is the " + zoo_animals[1]
+	print "The third animal at the zoo is the " + zoo_animals[2]
+	print "The fourth animal at the zoo is the " + zoo_animals[3]
+
+
+
+numbers = [5, 6, 7, 8]
+
+print "Adding the numbers at indices 0 and 2..."
+print numbers[0] + numbers[2]
+print "Adding the numbers at indices 1 and 3..."
+print numbers[1] + numbers[3]
+
+
+zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"]
+# Last night our zoo's sloth brutally attacked
+#the poor tiger and ate it whole.
+
+# The ferocious sloth has been replaced by a friendly hyena.
+zoo_animals[2] = "hyena"
+
+# What shall fill the void left by our dear departed tiger?
+zoo_animals[3] = "cat"
+
+
+suitcase = []
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("phone")
+suitcase.append("charger")
+suitcase.append("shoes")
+
+
+
+list_length = len(suitcase)# Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
+
+animals = "catdogfrog"
+cat  = animals[:3]   # The first three characters of animals
+dog  = animals[3:6]              # The fourth through sixth characters
+frog = animals[6:]              # From the seventh character to the end
+
+
+
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")    # Use index() to find "duck"
+print duck_index
+# Your code here!
+
+animals.insert(duck_index, "cobra")
+
+print animals # Observe what prints after the insert operation
+
+
+my_list = [1,9,3,8,5,7]
+
+for number in my_list:
+    print 2 * number # Your code here
+
+
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+# Your code here!
+
+for x in start_list:
+    square_list.append(x ** 2)
+
+square_list.sort()
+print square_list
+
+
+
+####python list and dictionaries ###
+
+# Assigning a dictionary with three key-value pairs to residents:
+residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+
+print residents['Puffin'] # Prints Puffin's room number
+
+# Your code here!
+print residents['Sloth']
+print residents['Burmese Python']
+
+
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Spam'] = 12
+menu['Burger'] = 29.12
+menu['potato'] = 35.12
+
+
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+
+
+
+# key - animal_name : value - location
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+del zoo_animals['Sloth']
+del zoo_animals['Bengal Tiger']
+
+# Your code here!
+zoo_animals['Rockhopper Penguin'] = 'Entry House'
+
+
+
+print zoo_animals
+
+
+
+###removing item from list ####
+
+backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+backpack.remove("dagger")
+
+
+
+
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort()
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+inventory['backpack'].sort()
+print inventory['backpack']
+inventory['backpack'].remove("dagger")
+inventory['gold'] = inventory['gold'] + 50
+
+
+names = ["Costas","Adam","Alex","Mariah","Martine","Columbus"]
+
+names.sort()
+
+for name in names:
+    print name
+
+
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# Add your code below!
+
+for key in webster:
+    print webster[key]
+
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+for x in a:
+    if x % 2 == 0:
+        print x
+
+
+for letter in "Codecademy":
+    print letter
+
+# Empty lines to make the output pretty
+print
+print
+
+word = "Programming is fun!"
+
+for letter in word:
+    # Only print out the letter i
+    if letter == "i":
+        print letter
+
+
+
+
+prices = {
+"banana": 4,
+"apple": 2,
+"orange": 1.5,
+"pear": 3
+}
+
+stock = {
+"banana": 6,
+"apple": 0,
+"orange": 32,
+"pear": 15,
+}
+
+for key in prices:
+    print "%s" % key
+    print "price: %s" % prices[key]
+    print "stock: %s" % stock[key]
+
+
+
+prices = {
+    "banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3,
+}
+stock = {
+    "banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15,
+}
+total = 0
+
+for key in prices:
+    print key
+    money = prices[key] * stock[key]
+    print "Money= %s" % money
+    total = total + money
+
+print total
