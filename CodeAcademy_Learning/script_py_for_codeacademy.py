2409ee82ee14e05aed8b4e0531a5e6b5fe15baca
Constantine <cdiam@windowslive.com>
2017-03-05 17:12:17 +0200

    Added more lines of code in script_py file

--- CodeAcademy_Learning/script_py_for_codeacademy.py
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -18,3 +18,135 @@ meal = meal + meal * tax
 total = meal + (meal * tip)
 
 print("%.2f" % total)
+
+
+#print letter
+
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
+
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+
+
+# Print the concatenation of "Spam and eggs" on line below!
+
+print"Spam "+"and "+"eggs"
+
+# Turn 3.14 into a string on line below!
+
+print "The value of pi is around " + str(3.14)
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
+
+
+
+# Write your code below, starting on line below!
+
+my_string = "Constantine"
+print len(my_string)
+print my_string.upper()
+
+
+
+###########DATE AND TIME ################
+
+from datetime import datetime
+now = datetime.now()
+
+print now
+print now.year
+print now.month
+print now.day
+
+print '%s/%s/%s' % (now.month, now.day, now.year)
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
+
+print '%s/%s/%s %s:%s:%s' % (now.month,now.day,now.year,now.hour, now.minute, now.second)
+
+
+
+#############SECTION Conditionals & Control Flow ##################
+
+def clinic():
+    print "You've just entered the clinic!"
+    print "Do you take the door on the left or the right?"
+    answer = raw_input("Type left or right and hit 'Enter'.").lower()
+    if answer == "left" or answer == "l":
+        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+    elif answer == "right" or answer == "r":
+        print "Of course this is the Argument Room, I've told you that already!"
+    else:
+        print "You didn't pick left or right! Try again."
+        clinic()
+
+clinic()
+
+
+
+# Assign True or False as appropriate on the lines below!
+
+# Set this to True if 17 < 328 or to False if it is not.
+bool_one = True   # We did this one for you!
+
+# Set this to True if 100 == (2 * 50) or to False otherwise.
+bool_two = True
+
+# Set this to True if 19 <= 19 or to False if it is not.
+bool_three = True
+
+# Set this to True if -22 >= -18 or to False if it is not.
+bool_four = False
+
+# Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = False
+
+
+"""
+     Boolean Operators
+------------------------      True and True is True
+True and False is False
+False and True is False
+False and False is False
+
+True or True is True
+True or False is True
+False or True is True
+False or False is False
+
+Not True is False
+Not False is True
+
+"""
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = (1+1) == 2 and (2+2)==4
+
+# Make me false!
+bool_three = (5-0)<1 or "my"=="your"
+
+# Make me true!
+bool_four = True and not not True
+
+# Make me true!
+bool_five = (2+2)==4 or (5+5)==10
