c33bb8606e8ca1511ad7f62900cbad64b3b24265
Constantine <cdiam@windowslive.com>
2017-03-05 16:39:49 +0200

    Changed to executables all old files and added new file script_py_for_codeacademy for traking the lessson

---
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -0,0 +1,20 @@
+#This is the scripts from codeacademy
+#Set spam equal to 1 using modulo on line 3!
+
+spam =3%2
+
+span2 = 4%2
+
+print "The first is %d and the second is %d" % (spam, span2)
+
+
+# Tip Calculator!
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal + (meal * tip)
+
+print("%.2f" % total)
