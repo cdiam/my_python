79abb07e8682ba738efb1d3227aaac51821a1c7c
Constantine <cdiam@windowslive.com>
2017-03-06 23:08:34 +0200

    more code

--- CodeAcademy_Learning/script_py_for_codeacademy.py
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -648,3 +648,34 @@ def double_list(x):
 # Don't forget to return your new list!
 
 print double_list(n)
+
+
+
+def my_function(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+
+print my_function(range(3)) # Add your range between the parentheses!
+
+
+n = [3, 5, 7]
+
+def total(numbers):
+    result = 0
+    for i in range(len(numbers)):
+        result = result + numbers[i]
+    return result
+
+
+n = ["Michael", "Lieberman"]
+# Add your function here
+
+def join_strings(words):
+    result = ""
+    for i in range(len(words)):
+        result = result + words[i]
+    return result
+
+
+print join_strings(n)
