2bdb0971fbd32e452ff157b4ad00ae98a09f80b5
Constantine <cdiam@windowslive.com>
2017-03-06 22:40:47 +0200

    Coding continues

--- CodeAcademy_Learning/script_py_for_codeacademy.py
+++ CodeAcademy_Learning/script_py_for_codeacademy.py
@@ -638,3 +638,13 @@ def print_list(x):
         print x[i]
 
 print print_list(n)
+
+n = [3, 5, 7]
+
+def double_list(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+# Don't forget to return your new list!
+
+print double_list(n)
