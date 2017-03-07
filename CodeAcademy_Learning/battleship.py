4b0581d344a485da543ed4954246914261cfed55
Constantine <cdiam@windowslive.com>
2017-03-07 21:51:29 +0200

    Battleship file in python Codeacademy

---
+++ CodeAcademy_Learning/battleship.py
@@ -0,0 +1,51 @@
+###Python battleship
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(4):
+    print "Turn", turn + 1
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    print ship_row
+    print ship_col
+
+
+    # Write your code below!
+    if guess_row == ship_row and guess_col == ship_col:
+        print"Congratulations! You sank my battleship!"
+        break   #End of the program if user correctly finds the location of battleship
+    else:
+        if (turn == 3): #if all turns have passed, game is over, end of program!
+            print 'Game Over, you have used your all turns.\n'
+        elif (guess_row < 0 or guess_row > 4 or guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif board[guess_row][guess_col] == "x":
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            print guess_row
+            print guess_col
+            board[guess_row][guess_col] ="X"
+            print 'You have used %d turn(s)' % (turn + 1) #turn+1 because user must not get turn 0,1,2 and 3 instead the user must get turn 1,2,3 and 4.
+
+            print_board(board)
