c3988866937f61e700e5c6b2a286bb1b7f7fac0a
Constantine <cdiam@windowslive.com>
2017-03-10 22:58:52 +0200

    More loops

--- CodeAcademy_Learning/Loops.py
+++ CodeAcademy_Learning/Loops.py
@@ -8,3 +8,24 @@ if count < 9:
 while count < 10:
     print "Hello, I am a while and count is", count
     count += 1
+
+
+
+num = 1
+
+while num < 11:  # Fill in the condition
+    print num**2 # Print num squared
+    num +=1 # Increment num (make sure to do this!)
+
+
+choice = raw_input('Enjoying the course? (y/n)')
+
+while choice !='y' and choice !='n':  # Fill in the condition (before the colon)
+    choice = raw_input("Sorry, I didn't catch that. Enter again: ")
+
+
+count = 0
+
+while count < 10: # Add a colon
+    print count
+    count +=1# Increment count
