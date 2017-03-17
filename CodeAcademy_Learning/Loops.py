69e2065a8a2bd16357b03c646bcbdea192249102
Constantine <cdiam@windowslive.com>
2017-03-17 06:46:20 +0200

    Loops done

--- CodeAcademy_Learning/Loops.py
+++ CodeAcademy_Learning/Loops.py
@@ -161,3 +161,24 @@ for a, b in zip(list_a, list_b):
         print b
     else:
         print "equal"
+
+
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+    if f == 'tomato':
+        print 'A tomato is not a fruit!' # (It actually is.)
+        break
+    print 'A', f
+else:
+    print 'A fine selection of fruits!'
+
+
+players = [ 'Ronaldo', 'Mesi', 'Zlatan' , 'Ramos']
+
+for f in players:
+    print f, " "
+
+else:
+    print "done"
